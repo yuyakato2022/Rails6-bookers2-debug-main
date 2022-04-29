84c2546db7b5de1bef93f49e00b47a8c4202919c
yuyakato2022 <102748219+yuyakato2022@users.noreply.github.com>
2022-04-29 13:31:34 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,13 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @booknew = Book.new
     @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def index
     @books = Book.all
     @book = Book.new
+
   end
 
   def create
